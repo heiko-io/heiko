f30d4b3729002d4f485c3b746a6dbfe3d364f362
Pranav Kesavarapu <pranavkesavarapu@gmail.com>
2024-03-18 19:43:28 +0530

    feat: initial connection test

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/heiko-io/heiko
+
+go 1.21.7
+
+require (
+	github.com/schollz/peerdiscovery v1.7.2 // indirect
+	golang.org/x/net v0.22.0 // indirect
+	golang.org/x/sys v0.18.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/schollz/peerdiscovery"
+)
+
+func main() {
+	discoveries, err := peerdiscovery.Discover(peerdiscovery.Settings{ Limit: 0, Port: "5767", Payload: []byte("Hello There!") })
+	if err != nil {
+		panic(err)
+	}
+	
+	if len(discoveries) > 0 {
+		fmt.Printf("Found %d other computers\n", len(discoveries))
+		for i, d := range discoveries {
+			fmt.Printf("%d) '%s' with payload '%s'\n", i, d.Address, d.Payload)
+		}
+	} else {
+		fmt.Println("No devices found :(")
+	}
+}
